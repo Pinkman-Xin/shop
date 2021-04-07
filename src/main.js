88dacc49999aa69b2bba69fa770820e772c0cc3e
pinkx <707479101@qq.com>
2021-04-07 16:41:33 +0800

    完成了登录页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import './assets/css/global.css'
+import './elementui/element.js'
+import './assets/fonts/iconfont.css'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
